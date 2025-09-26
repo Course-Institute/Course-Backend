6d78f8a27a81d92d0e39ee4ab459160a399829da
VibhanshuDigitalPaani <vibhanshu.vaibhav@digitalpaani.com>
2025-09-26 18:26:44 +0000

    added pm2 config file 2

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -3,16 +3,8 @@ module.exports = {
     {
       name: 'node-backend',
       script: 'dist/app.js',
-      interpreter: 'node',
       instances: 1,
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5000
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 5000
-      }
+      exec_mode: 'fork',
     }
   ]
 };
