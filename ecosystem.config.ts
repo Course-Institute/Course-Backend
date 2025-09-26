bd2668131a034a60146b840c2ae0388cda98fd4c
VibhanshuDigitalPaani <vibhanshu.vaibhav@digitalpaani.com>
2025-09-26 18:29:32 +0000

    added pm2 config file 3

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,10 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'node-backend',
-      script: 'dist/app.js',
-      instances: 1,
-      exec_mode: 'fork',
-    }
-  ]
+      name: "node-backend", // Name of your application
+      script: "./dist/app.js", // Entry point of your application
+      restart_delay: 5000, // Delay before restarting crashed instances
+      watch: false, // Set to true if you want to watch for file changes
+      // Uncomment and configure if needed:
+      instance_var: "INSTANCE_ID",
+      instances: 1, // Fixed number of instances
+      max_memory_restart: "500M", // Restart if memory exceeds this limit
+    },
+  ],
 };
