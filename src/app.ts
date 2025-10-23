a4bb2b60c7411a1a6a1e7d845e690ca1de1f7bfd
VibhanshuDigitalPaani <vibhanshu.vaibhav@digitalpaani.com>
2025-10-23 10:10:27 +0000

    added centers apis 2

--- src/app.ts
+++ src/app.ts
@@ -14,35 +14,32 @@ dotenv.config();
 connectDB();
 
 const app = express();
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
-//     console.log("Request Origin:", origin)
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    console.log("Request Origin:", origin)
     
-//     // Allow specific ports
-//     const allowedOrigins = [
-//       'http://localhost:4000',
-//       'http://127.0.0.1:4000',
-//       'http://localhost:5173',
-//       'http://127.0.0.1:5173',
-//       'http://localhost:5000',
-//       'http://127.0.0.1:5000'
-//     ];
+    // Allow specific ports
+    const allowedOrigins = [
+      'http://localhost:4000',
+      'http://127.0.0.1:4000',
+      'https://mivpsa.in'
+    ];
     
-//     if (allowedOrigins.includes(origin)) {
-//       return callback(null, true);
-//     }
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
     
-//     return callback(new Error('Not allowed by CORS'));
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers'],
-//   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
-//   credentials: true,
-//   preflightContinue: false,
-//   optionsSuccessStatus: 200
-// }));
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers'],
+  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
